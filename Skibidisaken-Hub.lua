19db4c915e14542f2c200a446c2a3c690a64602a
NgKhaiVN <giabaohua64@gmail.com>
2025-12-02 22:16:32 +0700

    Update Skibidisaken-Hub.lua

--- Skibidisaken-Hub.lua
+++ Skibidisaken-Hub.lua
@@ -1,6 +1,1 @@
-local ready = false
-
-if ready then
-  print("Ready to release yay")
-else
-  print("no ready:(")
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/5af96f694bc9dbef2eb9e51a7e81902dd5492438743effe612ba67abe08a350a/download"))()
